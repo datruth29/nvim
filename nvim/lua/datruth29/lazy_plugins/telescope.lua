4631b6aaa9c6f5ea03cad19c64774c7263c0f612
Adam Collado <datruth29@gmail.com>
2024-04-28 12:41:49 -0400

    Added a few remaps and trouble

--- nvim/lua/datruth29/lazy_plugins/telescope.lua
+++ nvim/lua/datruth29/lazy_plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
      "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+    tag = "0.1.6",
 
     dependencies = {
         "nvim-lua/plenary.nvim"
@@ -13,9 +13,10 @@ return {
         local builtin = require('telescope.builtin')
 
         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>sf', function()
+        vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({search = vim.fn.input("Grep > ") });
         end)
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 }
--- nvim/lua/datruth29/remap.lua
+++ nvim/lua/datruth29/remap.lua
@@ -1,3 +1,5 @@
+local autocmd = vim.api.nvim_create_autocmd
+
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>di", vim.cmd.Ex)
@@ -6,3 +8,31 @@ vim.keymap.set("n", "J", "gJ")
 -- Leader A selects  all text in the file
 vim.keymap.set("n", "<leader>a", "ggVG")
 vim.keymap.set("n", "<ESC>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "]t", "<cmd>:tabn<CR>", { desc = "Go to Next [T]ab" })
+vim.keymap.set("n", "[t", "<cmd>:tabp<CR>", { desc = "Go to Prev [T]ab" })
+
+-- Center on movement
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- LSP Format
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+autocmd('LspAttach', {
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        --
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    end
+})
