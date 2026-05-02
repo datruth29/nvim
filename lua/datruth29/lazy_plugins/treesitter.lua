53f340e897623bfb3ebfcc899a87e2924f6dec6f
Adam Collado <adam.collado@gmail.com>
2026-05-02 14:08:45 -0400

    Updated lazy for neovim 12

--- lua/datruth29/lazy_plugins/treesitter.lua
+++ lua/datruth29/lazy_plugins/treesitter.lua
@@ -1,26 +1,31 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
     config = function()
-        require("nvim-treesitter").setup({
-            ensure_installed = { "javascript", "typescript", "python", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        local parsers = {
+            "javascript",
+            "typescript",
+            "python",
+            "c",
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
+            "markdown",
+            "markdown_inline",
+        }
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+        local group = vim.api.nvim_create_augroup("datruth29-treesitter", { clear = true })
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-            ignore_install = {},
+        require("nvim-treesitter").install(parsers)
 
-            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-            highlight = {
-                enable = true,
-
-                additional_vim_regex_highlighting = false,
-            },
+        vim.api.nvim_create_autocmd("FileType", {
+            group = group,
+            pattern = parsers,
+            callback = function(args)
+                pcall(vim.treesitter.start, args.buf)
+            end,
         })
     end
 
