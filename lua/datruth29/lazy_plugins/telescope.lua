c9366780b1304b3c1982c0371e7587b8d2e93b2a
Adam Collado <adam.collado@gmail.com>
2026-05-02 18:54:53 -0400

    Updated telescope

--- lua/datruth29/lazy_plugins/telescope.lua
+++ lua/datruth29/lazy_plugins/telescope.lua
@@ -1,14 +1,19 @@
 return {
      "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.6",
+    version = "*",
 
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+        },
     },
 
     config = function()
         require("telescope").setup({})
+        pcall(require("telescope").load_extension, "fzf")
 
         local builtin = require('telescope.builtin')
 
