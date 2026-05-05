c2573304b282f748766ae659453838e3dcb90ecd
Adam Collado <adam.collado@gmail.com>
2026-05-04 22:46:45 -0400

    Update treesitter file types

--- lua/datruth29/lazy_plugins/treesitter.lua
+++ lua/datruth29/lazy_plugins/treesitter.lua
@@ -16,13 +16,25 @@ return {
             "markdown_inline",
         }
 
+        local filetypes = {
+            "javascript",
+            "typescript",
+            "python",
+            "c",
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
+            "markdown",
+        }
+
         local group = vim.api.nvim_create_augroup("datruth29-treesitter", { clear = true })
 
         require("nvim-treesitter").install(parsers)
 
         vim.api.nvim_create_autocmd("FileType", {
             group = group,
-            pattern = parsers,
+            pattern = filetypes,
             callback = function(args)
                 pcall(vim.treesitter.start, args.buf)
             end,
