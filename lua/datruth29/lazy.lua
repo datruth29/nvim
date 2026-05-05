cdd5986b3a6c4ba86aa2a7e22abadc6ed6568521
Adam Collado <adam.collado@gmail.com>
2026-05-04 21:21:09 -0400

    Fixed bug in lazy

--- lua/datruth29/lazy.lua
+++ lua/datruth29/lazy.lua
@@ -3,7 +3,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
         { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
